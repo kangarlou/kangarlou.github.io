4205c3c34687990ebb84296911d31b17a7ca4eb0
Ardalan Kangarlou <ardalan@Ardalans-MacBook-Pro.local>
2015-12-07 18:14:47 -0500

    - Setting up a golang dev environment

--- vagrant/Ubuntu-14.04LTS/bootstrap.sh
+++ vagrant/Ubuntu-14.04LTS/bootstrap.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git build-essential cmake python-dev
+apt-get install -y git build-essential cmake python-dev golang
 mkdir -p /home/vagrant/.vim/autoload /home/vagrant/.vim/bundle
+mkdir -p /home/vagrant/go
+echo -n >  /etc/profile.d/gopath.sh
+echo 'export GOPATH=$HOME/go' >> /etc/profile.d/gopath.sh
+echo 'export PATH=$PATH:$GOPATH/bin' >> /etc/profile.d/gopath.sh
 git clone https://github.com/VundleVim/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
 git clone https://github.com/fatih/vim-go.git /home/vagrant/.vim/bundle/vim-go
 ln -sF /vagrant/vimrc.local /home/vagrant/.vimrc
-chown vagrant:vagrant /home/vagrant/.vimrc
-chown -R vagrant:vagrant /home/vagrant/.vim
+chown -R vagrant:vagrant /home/vagrant
 
 # manual steps after 'vagrant up'
 #vim +PluginInstall +qall
-#/home/vagrant/.vim/bundle/YouCompleteMe/install.py --clang-completer
+#$HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
+#vim +GoInstallBinaries +qall 
